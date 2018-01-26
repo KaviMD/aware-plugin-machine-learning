07d57b6dbd5dd4050aa3d99589775e87f748afff
KaviMD <kaviasher@gmail.com>
2018-01-26 16:17:28 -0500

    Fixed Standard Deviaton and Correlation
    Fixed Standard Deviaton and Correlation in FeatureMath

--- com.aware.plugin.template/src/main/java/com/aware/plugin/template/FeatureMath.java
+++ com.aware.plugin.template/src/main/java/com/aware/plugin/template/FeatureMath.java
@@ -1,12 +1,9 @@
 package com.aware.plugin.template;
 import org.jtransforms.fft.DoubleFFT_1D;
+import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
+import org.apache.commons.math3.stat.correlation.PearsonsCorrelation;
 import java.lang.Math;
-import android.util.Log;
-//import org.apache.commons.math3.stat.correlation.PearsonsCorrelation;
-
-/**
- * Created by kavidey on 1/5/18.
- */
+import java.util.Arrays;
 
 public class FeatureMath {
     public static double Sum(double[] doubles) {
@@ -22,11 +19,8 @@ public class FeatureMath {
         return sum / m.length;
     }
     public static double STD(double[] data) {
-        double mean = Mean(data);
-        for(int i = 0; i < data.length; i++){
-            data[i] = Math.pow((data[i]-mean),2);
-        }
-        return Math.sqrt(Sum(data));
+        StandardDeviation stdev = new StandardDeviation();
+        return stdev.evaluate(data);
     }
 
     public static double Energy(double[] data, int winSize) {
@@ -40,42 +34,21 @@ public class FeatureMath {
             }
         }
         double sum = 0.0;
-        Log.i("data.length",String.valueOf(data2.length));
         DoubleFFT_1D fftCl = new DoubleFFT_1D(data.length);
         fftCl.realForwardFull(data2);
-        for (int i=1; i<data2.length;i++){
-            sum = sum + Math.pow(data2[i],2);
+        System.out.println(Arrays.toString(data2));
+        for (int i=0; i<data2.length;i++){
+            if(i%2 == 0){
+                sum = sum + Math.pow(data2[i],2);
+            } else {
+                sum = sum - Math.pow(data2[i],2);
+            }
+            System.out.println(sum);
         }
-        return sum/winSize; // 63
+        return sum/winSize; // winSize should be 63
     }
     public static double Correlation(double[] x,double[] y){
-        // Save the x and y means in variables
-        double xMean = Mean(x);
-        double yMean = Mean(y);
-
-        // Create empty variables to store the numerator and denominator
-        double numerator = 0;
-        double denominator = 0;
-
-        // Calculate the numerator
-        for(int i = 0;i < x.length;i++) {
-            numerator = numerator + (x[i]-xMean)*(y[i]-yMean);
-        }
-
-        // Calculate the denominator
-        double xSum = 0;
-        double ySum = 0;
-        for(int i = 0;i < x.length;i++){
-            xSum = xSum + x[i]-xMean;
-        }
-        xSum = Math.pow(xSum,2);
-        for(int i = 0;i < x.length;i++) {
-            ySum = ySum + y[i] - yMean;
-        }
-        ySum = Math.pow(ySum,2);
-        denominator = xSum*ySum;
-        //PearsonsCorrelation corr = new PearsonsCorrelation();
-        //return corr.correlation(x, y);
-        return denominator;
+        PearsonsCorrelation PC = new PearsonsCorrelation();
+        return ((PC.correlation(x,y)*2)+(2))/4;
     }
 }
